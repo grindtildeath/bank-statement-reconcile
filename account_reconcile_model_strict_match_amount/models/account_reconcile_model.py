76993ab9d63e3461b5aee29ca72261489f0386dc
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-01-23 14:37:52 +0100

    fixup! fixup! [ADD] account_reconcile_model_strict_match_amount

--- account_reconcile_model_strict_match_amount/models/account_reconcile_model.py
+++ account_reconcile_model_strict_match_amount/models/account_reconcile_model.py
@@ -18,24 +18,25 @@ class AccountReconcileModel(models.Model):
         if not self.match_total_amount or not self.strict_match_total_amount:
             return super()._get_select_communication_flag()
         else:
-            return '''
+            regexp = r"'[^0-9|^\s]', '', 'g'), '\S(?:.*\S)*'), '\s+'"
+            return r'''
                 -- Determine a matching or not with the statement line communication using the move.name or move.ref.
                 -- only digits are considered and reference are split by any space characters
                 COALESCE(
-                    regexp_split_to_array(substring(REGEXP_REPLACE(move.name, '[^0-9|^\s]', '', 'g'), '\S(?:.*\S)*'), '\s+')
-                    && regexp_split_to_array(substring(REGEXP_REPLACE(st_line.name, '[^0-9|^\s]', '', 'g'), '\S(?:.*\S)*'), '\s+')
+                    regexp_split_to_array(substring(REGEXP_REPLACE(move.name, {regexp})
+                    && regexp_split_to_array(substring(REGEXP_REPLACE(st_line.name, {regexp})
                     OR
                     (
                         move.ref IS NOT NULL
                         AND
-                            regexp_split_to_array(substring(REGEXP_REPLACE(move.ref, '[^0-9|^\s]', '', 'g'), '\S(?:.*\S)*'), '\s+')
+                            regexp_split_to_array(substring(REGEXP_REPLACE(move.ref, {regexp})
                             &&
-                            regexp_split_to_array(substring(REGEXP_REPLACE(st_line.name, '[^0-9|^\s]', '', 'g'), '\S(?:.*\S)*'), '\s+')
-                ), TRUE)
+                            regexp_split_to_array(substring(REGEXP_REPLACE(st_line.name, {regexp})
+                ), FALSE)
                 AND
                 CASE
-                    WHEN abs(st_line.amount) < abs(aml.balance) THEN st_line.amount / aml.balance * 100
-                    WHEN abs(st_line.amount) > abs(aml.balance) THEN aml.balance / st_line.amount * 100
+                    WHEN abs(st_line.amount) < abs(aml.balance) THEN abs(st_line.amount) / abs(aml.balance) * 100
+                    WHEN abs(st_line.amount) > abs(aml.balance) THEN abs(aml.balance) / abs(st_line.amount) * 100
                     ELSE 100
-                END >= %s AS communication_flag
-            ''' % self.match_total_amount_param
+                END >= {match_total_amount_param} AS communication_flag
+            '''.format(regexp=regexp, match_total_amount_param=self.match_total_amount_param)
